d49abe1bf5da689750fade4f28b696201a80138b
Annika Grothaus <annika.grothaus@gmx.de>
2022-09-30 17:01:13 +0200

    snake shiftin code

--- src/main/java/com/game/chordsnake/model/Snake.java
+++ src/main/java/com/game/chordsnake/model/Snake.java
@@ -1,5 +1,6 @@
 package com.game.chordsnake.model;
 
+import java.lang.annotation.Documented;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +10,7 @@ public class Snake {
     private int direction; // up=0, left=1, right=2, down=3
 
     //position of Snake
-    private final List<int[]> snakePosition = new ArrayList<>();
+    private List<int[]> snakePosition = new ArrayList<>();
 
     public Snake(int head_y, int head_x) {
         int[] coords = {head_x, head_y};
@@ -25,21 +26,37 @@ public class Snake {
         //TODO play sound
     }
 
+    //shifts snake forward one field
     public void shiftSnake() {
-        //TODO
-
-        // according to direction calculate new snake head (snakePosition[0])
-
-        // shift everything in snakePosition one index behind
-
-        //set snakePosition[0] to calculated direction
-
+        List<int[]> tmp = snakePosition;
+        int [] newHeadPosition = null;
+        if (direction == 0){ //up
+            newHeadPosition = new int[]{snakePosition.get(0)[0], snakePosition.get(0)[1] + 1};
+        }
+        else if (direction == 3){ //down
+            newHeadPosition = new int[]{snakePosition.get(0)[0], snakePosition.get(0)[1] - 1};
+        }
+        else if (direction == 2){ //right
+            newHeadPosition = new int[]{snakePosition.get(0)[0] + 1, snakePosition.get(0)[1]};
+        }
+        else if (direction == 1){ //left
+            newHeadPosition = new int[]{snakePosition.get(0)[0] - 1, snakePosition.get(0)[1]};
+        }
+        snakePosition.set(0, newHeadPosition);
+        for (int i = 0; i<snakePosition.size(); i++){
+            snakePosition.set(i+1, tmp.get(i));
+        }
     }
 
+    //clears snake and leaves only the snake head
     public void clearSnake() {
         snake = new ArrayList<String>();
         snake.add("SH");
 
+    }
+
+    public List<String> getSnake() {
+        return snake;
     }
 
     public void setSnakeDirectionRight() {
@@ -62,7 +79,4 @@ public class Snake {
         return direction;
     }
 
-    public List<String> getSnake() {
-        return snake;
-    }
 }
